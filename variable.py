c11d024a4eedc9ebb918ffac3f2d49c08ad75db5
JsCore <thanhfgit@gmail.com>
2018-12-09 06:36:06 -0500

    variables done

---
+++ variable.py
@@ -0,0 +1,22 @@
+
+# x           = 2             # int
+# y           = 2.5           # float
+# name        = 'Hello'       # string
+# is_cool     = True          # boolean
+
+# Multi assign value for variables
+# x, y, name, is_cool = (1, 2.5, 'Hello', True)
+## => output 
+#print('Hello World')
+
+# if 2 < 1:
+#     print(' 2 > 1 dung')
+# elif 3 > 1:
+#     print(' 3 > 1 dung')
+# else:
+
+num, book_prices, flag, name = (100, 12.5, True, "Nhat Thanh")
+print(type(int(book_prices)))
+int(book_prices)
+print(type(book_prices))
+
